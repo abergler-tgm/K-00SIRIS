d2e99f7e997b5f8fcc3024dcba9bb7bc90f9a53a
Adrian Bergler <adrian.bergler@aon.at>
2016-08-31 15:46:54 +0200

    Improved doc and made some minor changes

--- src/robotarm.py
+++ src/robotarm.py
@@ -8,12 +8,8 @@ from kinematics import Kinematics, KinematicsError, WorldCoordinateSystem
 class Robotarm:
     """
     Main controller-class for the 00SIRIS Robotarm
-    """
-
-    # TODO: Integrate Hedgehog and get this going
-    """
-    Hedgehog information:
-    2000 Steps per Servo
+        Hedgehog information:
+            -2000 Steps per Servo
     """
 
     def __init__(self, client, links, base_offset, min_angles, max_angles, min_servo_pos, max_servo_pos):
@@ -40,9 +36,18 @@ class Robotarm:
         self.kinematics = Kinematics(self.links, self.base_offset)
 
     def move_to_init(self):
+        """
+        Moves the robotarm to the initial position
+        """
         self.move_to_config([0.0, 1.51, -1.51, 0.0, 0.0, 0.0])
 
     def move_to_pos(self, pos, joint):
+        """
+        Moves the given joint to the given position
+        :param pos: the position
+        :param joint: the joint that will be used
+        :raise OutOfReachError: if the given position cannot be reached
+        """
         if 0 <= pos <= 2000:
             self.client.set_servo(joint, True, int(pos))
             self.joint_pos[joint] = pos
@@ -74,25 +79,34 @@ class Robotarm:
 
         pos = (total_steps / (2 * math.pi)) * angle + self.min_servo_pos[joint]
 
-        # TODO test properly
+        # TODO: Test properly
+        # TODO: Probably very buggy
 
         return pos
 
-    def step_to_angle(self, step, joint):
-        if self.min_servo_pos[joint] < step < self.max_servo_pos[joint]:
+    def step_to_angle(self, pos, joint):
+        """
+        Calculates the angle of the given servo-position (in steps) for the given joint.
+        Servos have 2048 steps and this maps the angle to those
+        :param pos: the given step
+        :param joint: the joint that should be used
+        :return: the servo-position in angle
+        """
+        if self.min_servo_pos[joint] < pos < self.max_servo_pos[joint]:
             raise OutOfReachError
 
         total_steps = (self.max_servo_pos[joint] - self.min_servo_pos[joint])
 
-        angle = ((2 * math.pi) / total_steps) * (step - self.min_servo_pos[joint])
+        angle = ((2 * math.pi) / total_steps) * (pos - self.min_servo_pos[joint])
+
+        # TODO: Test properly
+        # TODO: Probably very buggy
 
         return angle
 
     def move_to_cartesian(self, x, y, z, fixed_joint, fj_angle):
         """
         Moves the robotarm to the given cartesian coordinates.
-
-
         :param x: the x-coordinate
         :param y: the y-coordinate
         :param z: the z-coordinate
@@ -114,7 +128,6 @@ class Robotarm:
         """
         Moves the robotarm to the given cartesian coordinates.
         This one takes the alignment of the grabber towards the x-axis and uses it as the fixed joint.
-
         :param x: the x-coordinate
         :param y: the y-coordinate
         :param z: the z-coordinate
@@ -133,7 +146,6 @@ class Robotarm:
     def move_to_config(self, angles):
         """
         Move the robotarm to the given configuration.
-
         :param angles: the angles of all joints
         """
 
@@ -146,7 +158,6 @@ class Robotarm:
     def validate_configuration(self, angles):
         """
         Validates whether the given configuration is reachable.
-
         :param angles: the angles of all joints
         :return: True if the given configuration is reachable, else: False
         """
@@ -165,7 +176,6 @@ class Robotarm:
     def validate_angle(self, angle, joint):
         """
         Validates whether the given angle is reachable with the given joint.
-
         :param angle: the angle that should be validated
         :param joint: the joint that the angle should be validated for
         :return: True if the given angle is reachable with the given joint, else: False
@@ -173,7 +183,12 @@ class Robotarm:
         return self.min_angles[joint] < angle < self.max_angles[joint]
 
     def move_servo_over_time(self, pos, joint, duration):
-
+        """
+        Moves a joint to the given position in exactly the given duration
+        :param pos: the position that the servo should move to
+        :param joint: the joint that should be moved
+        :param duration: the duration that the movement should take
+        """
         start_position = self.joint_pos[joint]
         movement = pos - start_position
 
@@ -186,7 +201,7 @@ class Robotarm:
         while start_time + duration > time.time() and pos != self.joint_pos[joint]:
             crnt_step = round((time.time() - start_time) / time_per_step)
             self.move_to_pos(start_position + crnt_step * math.copysign(1, movement), joint)
-            time.sleep(0.001)   # To prevent from overload
+            time.sleep(0.001)  # To prevent from overload
 
         """
         Not very beautiful, but this will ensure that the pos will definitely be reached even
@@ -195,6 +210,10 @@ class Robotarm:
         self.move_to_pos(pos, joint)
 
     def get_tool_cs(self):
+        """
+        Returns the tool coordinate system
+        :return: the tool coordinate system as WorldCoordinateSystem
+        """
         angles = []
         for joint, pos in enumerate(self.joint_pos):
             angles[joint] = self.step_to_angle(pos, joint)
@@ -202,13 +221,11 @@ class Robotarm:
         x, y, z, theta_x, theta_y, theta_z = self.kinematics.direct(angles)
         return WorldCoordinateSystem(x, y, z, theta_x, theta_y, theta_z)
 
-    def close(self):
-        self.client.close()
-
-
-class Joint:
-    def __init__(self, ):
-        pass
+    def servos_off(self):
+        """
+        Turns off all servos
+        """
+        self.client.set_multi_servo([(0, False, 0), (1, False, 0), (2, False, 0), (3, False, 0)])
 
 
 class RobotarmError(Exception):
@@ -226,4 +243,7 @@ class RobotarmError(Exception):
 
 
 class OutOfReachError(RobotarmError):
+    """
+    Is thrown when a point or position is out of reach
+    """
     pass
